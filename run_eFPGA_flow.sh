a322cbc041913b83851a38a67db9a183c5a1aa4d
Hsu, Po-Kai <po-kai.hsu@intel.com>
2024-06-12 16:21:22 -0700

    new file:   1 	new file:   openfpga_flow/openfpga_arch_eFPGA/k4_N4_40nm_cc_openfpga_gates_mem.xml 	modified:   openfpga_flow/tasks/eFPGA/adder_arch/config/task.conf 	modified:   openfpga_flow/tasks/eFPGA/basic_arch/config/task.conf 	new file:   openfpga_flow/tasks/eFPGA/basic_arch_123/config/counter.pcf 	new file:   openfpga_flow/tasks/eFPGA/basic_arch_123/config/fpga_io_location.xml 	new file:   openfpga_flow/tasks/eFPGA/basic_arch_123/config/gcd.pcf 	new file:   openfpga_flow/tasks/eFPGA/basic_arch_123/config/pinmap_k4_N4_tileable_40nm.csv 	new file:   openfpga_flow/tasks/eFPGA/basic_arch_123/config/task.conf 	new file:   openfpga_flow/tasks/eFPGA/mem_arch/config/counter.pcf 	new file:   openfpga_flow/tasks/eFPGA/mem_arch/config/fpga_io_location.xml 	new file:   openfpga_flow/tasks/eFPGA/mem_arch/config/gcd.pcf 	new file:   openfpga_flow/tasks/eFPGA/mem_arch/config/pinmap_k4_N4_tileable_40nm.csv 	new file:   openfpga_flow/tasks/eFPGA/mem_arch/config/task.conf 	new file:   openfpga_flow/vpr_arch_eFPGA/k4_N4_tileable_40nm_CLB2O_mem.xml 	modified:   run_eFPGA_flow.sh 	new file:   skywater-pdk 	deleted:    openfpga_flow/tasks/eFPGA/basic_arch_123/config/counter.pcf 	deleted:    openfpga_flow/tasks/eFPGA/basic_arch_123/config/fpga_io_location.xml 	deleted:    openfpga_flow/tasks/eFPGA/basic_arch_123/config/gcd.pcf 	deleted:    openfpga_flow/tasks/eFPGA/basic_arch_123/config/pinmap_k4_N4_tileable_40nm.csv 	deleted:    openfpga_flow/tasks/eFPGA/basic_arch_123/config/task.conf 	modified:   skywater-pdk (modified content) 	modified:   vtr-verilog-to-routing (new commits) 	modified:   yosys (new commits, modified content) 	modified:   yosys-plugins (untracked content)

--- run_eFPGA_flow.sh
+++ run_eFPGA_flow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ${eFPGA_PATH}/OpenFPGA
-python3 openfpga_flow/scripts/run_fpga_task.py eFPGA/basic_arch --debug --show_thread_logs
+python3 openfpga_flow/scripts/run_fpga_task.py eFPGA/mem_arch --debug --show_thread_logs
